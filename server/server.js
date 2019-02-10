b22a7019232768b6fbf59ed75a130cf222919b1e
Johnny Sanchez <johnny.ss@icloud.com>
2019-02-10 13:16:54 -0800

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'username',
-        text: 'some text',
-        createdAt: new Date().toTimeString()
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('New message created by client', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('New message created by client', message);
+        io.emit('newMessage', {         // emits to all connections
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
