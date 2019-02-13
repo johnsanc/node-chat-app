d689c56f1ff299d478f48dcc1ec4f3072fc7c434
Johnny Sanchez <johnny.ss@icloud.com>
2019-02-13 07:55:47 -0800

    Added update user list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -33,6 +33,15 @@ socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', (users) => {
+    let ol = jQuery('<ol></ol>');
+    users.forEach(function(user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+});
+
 socket.on('newMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
     let template = jQuery('#message-template').html();
